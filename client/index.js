fffcb1efd76f91c7e607730692792dd7aaa98388
Demarius Chrite <chrite.demarius@gmail.com>
2016-11-06 17:48:22 -0400

    Pass client to callback.

--- client/index.js
+++ client/index.js
@@ -1,29 +1,37 @@
 var SmtpClient = require('emailjs-smtp-client')
 
-var initiated = false,
-    client = new SmtpClient('127.0.0.1', 6425, {
-        ignoreTLS: true
-    })
-
-client.onidle = function () {
-    if (!initiated) {
-        initiated = true
-        console.log('client idle')
-        client.useEnvelope({
-            from: "testAddress@home.com",
-            to: ["testAddress@work.com"]
+function client (smtp, callback) {
+    var initiated = false,
+        client = new SmtpClient('127.0.0.1', 6425, {
+            ignoreTLS: true
         })
+
+    client.onidle = function () {
+        if (!initiated) {
+            initiated = true
+            console.log('client idle')
+            client.useEnvelope({
+                from: "testAddress@home.com",
+                to: ["testAddress@work.com"]
+            })
+        }
     }
-}
 
-client.onready = function (failed) {
-    if (failed.length) {
-        console.log('failed to connect to ', failed)
+    client.onready = function (failed) {
+        if (failed.length) {
+            console.log('failed to connect to ', failed)
+            callback(failed)
+        } else {
+            console.log('client ready')
+            callback(null, client)
+        }
     }
+
+    client.onerror = function (err) {
+        console.log('client error: ', err)
+    }
+
+    client.connect()
 }
 
-client.onerror = function (err) {
-    console.log('client error: ', err)
-}
-
-client.connect()
+module.exports = client
