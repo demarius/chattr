273ae46bafcaf957d8ab674d1764e2e96bfd9a2a
Demarius Chrite <chrite.demarius@gmail.com>
2016-11-10 15:27:01 -0500

    Build out test.

--- t/client.t.js
+++ t/client.t.js
@@ -2,4 +2,16 @@ require('proof')(1, prove)
 
 function prove (assert) {
     assert(true, 'Nothing yet')
+
+    var Server = require('../server/smtp.js')
+    var Client= require('../client/')
+
+    var server = Server({ port: 6425, address: '127.0.0.1' }).server
+
+    Client({ port: 6425, address: '127.0.0.1' }, function (error, client) {
+        client.close()
+        server.close(function () {
+            console.log('closed')
+        })
+    })
 }
