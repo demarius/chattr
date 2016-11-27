0f8de5f382a469b5dcc1a3d5712a529985e30832
Demarius Chrite <chrite.demarius@gmail.com>
2016-11-26 20:19:37 -0400

    Cleanup index.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var Server = require('./server/smtp.js')
-var Client= require('./client/')
+var Client = require('./client/')
 
 var app = Server({ port: 6425, address: '127.0.0.1' })
 
 Client({ port: 6425, address: '127.0.0.1' }, function (error, client) {
-    console.log(client)
     app.onConnect = function (session, callback) {
         //try overriding connect here
     }
